e2f86004d7664d4682115025f9dbb8b3ea7cb808
muhammadfaruqtursunov-alt <muhammadfaruqtursunov@gmail.com>
2026-05-09 00:45:25 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("8071015987:AAFSZPGArEgSx0Vzox-r5iosjD_V5P-a-Xk", "")
 DB_PATH = "arabic_bot.db"
 WORDS_PATH = "data/words.json"
 WORDS_PER_LESSON = 10
